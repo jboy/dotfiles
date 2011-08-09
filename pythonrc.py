c6b362973662b79a48a5d6ec3e1847c792289f81
James Boyden <github@jboy.id.au>
2011-08-10 01:43:18 +1000

    Extracted all the custom Readline bindings out of "pythonrc.py", so they can be reused elsewhere.

--- pythonrc.py
+++ pythonrc.py
@@ -9,141 +9,18 @@
 # See http://docs.python.org/using/cmdline.html#envvar-PYTHONSTARTUP
 
 import sys
-import types
-
 print >> sys.stderr, 'Loading configuration file "~/.pythonrc.py".'
-
-__bindings__ = {
-  # Each dictionary key is a key-sequence.
-  # The corresponding dictionary value is the target binding.
-  # If the value is a pair, the zeroth element is the target binding;
-  # the first element is a human-readable alias for the binding.
-
-  # Use Tab to trigger shell-like autocomplete.
-  'tab':            ('complete', 'tab-complete'),
-
-  # Use shift-Tab to insert a literal tab.
-  #
-  # "\e[Z" == "^[[Z" == shift-tab.
-  # To find out more exciting combinations like this, run "cat -v"
-  # and then press all your favourite non-ASCII keys.
-  r'"\e[Z"':        'tab-insert',
-
-  # Increase indentation of line using C-j (like C-t in Vim).
-  #
-  # Literally, this macro is: set-mark; beginning-of-line; insert-tab;
-  # exchange-point-and-mark; right-arrow (to compensate for the extra
-  # tab character inserted; Readline seems to track the set-mark position
-  # as a character-position offset).
-  r'"\C-x\C-m"':    ('set-mark', 'set-mark (then C-xC-x to exchange-point-and-mark)'),
-  r'C-j':           ('"\C-x\C-m\C-a\e[Z\C-x\C-x\e[C"', 'increase-indent'),
-
-  # Decrease indentation of line (the inverse of C-j).
-  #
-  # Literally, this macro is: left-arrow (since exchange-point-and-mark seems
-  # to become confused if the line is shorter than the set-mark position,
-  # which will occur if you were at the end of the line when you set the mark,
-  # and then you delete a character in the line); set-mark; beginning-of-line;
-  # right-arrow; backspace; exchange-point-and-mark.
-  #
-  # "\et" is the cryptic "keyseq" method of specifying Meta-t (Alt-t).
-  # It seems to be the only way to bind Meta/Alt on a modern Linux system.
-  # http://www.devheads.net/linux/fedora/user/readline-binding-still-frustrated-after-4-yrs-trying.htm
-  #
-  # Note that this binding also makes use of the binding of "\er" (Alt-r)
-  # to backward-delete-char that is performed later.
-  # Previously "\b" (the backspace character) was used, but for some reason
-  # it stopped working after the rebinding of Alt-r was added...  :\
-  #r'"\et"':         '"\e[D\C-x\C-m\C-a\e[C\b\C-x\C-x"',
-  #r'C-k':           '"\e[D\C-x\C-m\C-a\e[C\b\C-x\C-x"',
-  r'C-k':           ('"\e[D\C-x\C-m\C-a\e[C\er\C-x\C-x"', 'decrease-indent'),
-
-  # Bind the End key to menu-complete to cycle through possible completions,
-  # and the Home key to the reverse.
-  #r'"\eOF"':        'menu-complete',
-  #r'"\eOH"':        'menu-complete-backward',
-  r'C-t':           ('menu-complete', 'tab-complete-cycle-forward'),
-  # Next, I wanted to use C-s for the reverse of C-t, but it seems
-  # that either the Python interpreter or Readline just eats it...  :\
-  #r'C-s':           'menu-complete-backward',
-  r'"\et"':         ('menu-complete-backward', 'tab-complete-cycle-reverse'),
-
-  # Bind Meta-c (Alt-c) to insert a comment at the beginning of the line.
-  r'"\ec"':         'insert-comment',
-
-  # Swap the move-by-char and move-by-word commands.
-  r'C-f':           'forward-word',
-  r'C-b':           'backward-word',
-  r'"\ef"':         'forward-char',
-  r'"\eb"':         'backward-char',
-
-  # Swap the delete-char and delete-word commands.
-  r'C-r':           ('backward-kill-word', 'rubout-word (backward)'),
-  r'C-d':           ('kill-word', 'delete-word (forward)'),
-  r'"\er"':         ('backward-delete-char', 'rubout-char (backward)'),
-  r'"\ed"':         ('delete-char', 'delete-char (forward)'),
-
-  # Remap C-r and C-s character-search commands to C-a and C-e
-  r'"\ea"':         ('character-search-backward', 'char-search-forward (towards beginning of line)'),
-  r'"\ee"':         ('character-search', 'char-search-backward (towards end of line)'),
-
-  r'C-h':           'previous-history',
-  r'C-n':           'next-history',  # Already the default
-  r'"\eh"':         'reverse-search-history',
-  r'"\en"':         'forward-search-history',
-}
-
-
-def __get_readable_keyseq(k):
-  k = k.replace('"', '')
-
-  if k == 'tab':
-    return 'Tab'
-  if k == '\\e[Z':
-    return 'S-Tab'
-
-  k = k.replace('\\C', 'C')
-  k = k.replace('\\e', 'M-')
-
-  return k
-
-
-def __get_readable_binding(b):
-  if type(b) == types.TupleType:
-    return b[1]
-  else:
-    return b
-
-
-def __get_binding(b):
-  if type(b) == types.TupleType:
-    return b[0]
-  else:
-    return b
-
-
-def __bind_all(bindings, msg_stream):
-  for k, b in bindings.iteritems():
-    readline.parse_and_bind("%s: %s" % (k, __get_binding(b)))
-
-  # Remind myself that the mapping has been performed.
-  print >> msg_stream, 'Enabled key mappings:'
-  bindings = [(__get_readable_keyseq(k), __get_readable_binding(b))
-      for k, b in __bindings__.items()]
-  for k, b in sorted(bindings):
-    print >> msg_stream, '  % 8s  ->  %s' % (k, b)
-
 
 # Enable shell autocomplete for syntax completion.
 # See http://docs.python.org/tutorial/interactive.html
 # and the complete code in
 # http://www.razorvine.net/blog/user/irmen/article/2004-11-22/17
-try:
-  import readline
-except ImportError:
-  print >> sys.stderr, 'Warning: Module "readline" not available.'
-else:
-  import rlcompleter
+import readline
+import rlcompleter
 
-  __bind_all(__bindings__, sys.stderr)
+# This is a module in my PYTHONPATH.
+import custom_readline_bindings
+
+custom_readline_bindings.__bind_all(readline,
+  custom_readline_bindings.__bindings__, sys.stderr)
 
