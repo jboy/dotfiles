33eb20687c8802741eef070f6b752a219ddfa57d
James Boyden <james@snapdisco.com>
2016-02-14 21:24:32 +1100

    Make "~/.pythonrc" Python3-compatible.  Also, only load it in interactive mode.

--- pythonrc.py
+++ pythonrc.py
@@ -8,23 +8,34 @@
 #
 # See http://docs.python.org/using/cmdline.html#envvar-PYTHONSTARTUP
 
-import sys
-print >> sys.stderr, 'Loading configuration file "~/.pythonrc.py".'
+# Only load these config settings if the interpreter is in interactive mode;
+# or else the results can confuse the Cinnamon tools written in Python.
+# (This method of determining whether the interpreter is in interactive mode
+# is from http://stackoverflow.com/a/2356445 )
+#     If no interface option is given, -i is implied, sys.argv[0] is an
+#     empty string ("") and the current directory will be added to the
+#     start of sys.path.
+#      -- http://docs.python.org/2/using/cmdline.html
 
-# Enable shell autocomplete for syntax completion.
-# See http://docs.python.org/tutorial/interactive.html
-# and the complete code in
-# http://www.razorvine.net/blog/user/irmen/article/2004-11-22/17
-import readline
-import rlcompleter
+from __future__ import print_function
+import sys as _sys
 
-# This is a module in my PYTHONPATH.
-import custom_readline_bindings
+if _sys.argv[0] == "":
+    print('Loading configuration file "~/.pythonrc.py".', file=_sys.stderr)
 
-for binding in custom_readline_bindings.get_bindings().iteritems():
-  readline.parse_and_bind("%s: %s" % binding)
+    # Enable shell autocomplete for syntax completion.
+    # See http://docs.python.org/tutorial/interactive.html
+    # and the complete code in
+    # http://www.razorvine.net/blog/user/irmen/article/2004-11-22/17
+    import readline as _readline
+    import rlcompleter
 
-print >> sys.stderr, 'Enabled key mappings:'
-for binding in custom_readline_bindings.get_readable_bindings():
-  print >> sys.stderr, '  % 8s  ->  %s' % binding
+    # This is a module in my PYTHONPATH.
+    import custom_readline_bindings as _custom_readline_bindings
 
+    for _binding in _custom_readline_bindings.get_bindings().items():
+        _readline.parse_and_bind("%s: %s" % _binding)
+
+    print('Enabled key mappings:', file=_sys.stderr)
+    for _binding in _custom_readline_bindings.get_readable_bindings():
+        print('  % 8s  ->  %s' % _binding, file=_sys.stderr)
